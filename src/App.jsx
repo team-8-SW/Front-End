7a755eb53e66ead084f23254714b42cc3e3a5f62
youssef <youssefmansi2@gmail.com>
2025-03-11 21:03:28 +0200

    setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Button } from "@material-tailwind/react";
+
+function App() {
+  
+
+  return (
+    <div>
+      <p>hellooo</p>
+    </div>
+  )
+}
+
+export default App

247ebde19217c6f5247bdaa572d57e5291db8165
youssef <youssefmansi2@gmail.com>
2025-03-15 16:56:26 +0200

    y

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from "@material-tailwind/react";
+import Nav from './components/Nav'; 
 
 function App() {
   
 
   return (
     <div>
-      <p>hellooo</p>
+      <div className='w-full'>  <Nav /></div>
+     
+      
     </div>
   )
 }

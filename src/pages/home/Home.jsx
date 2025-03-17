e3516a47be10e1f330fe5145af8bd95920bec887
Mark-2005 <markmagednagek@gmail.com>
2025-03-17 12:26:18 +0200

    Create Post Bar

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import CreatePost from "./CreatePost";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <CreatePost />
+    </div>
+  );
+};
 
-export default Home
+export default Home;

f8c6ab8e40aa3d00d3a3d57063133aa91b85a394
Mark-2005 <markmagednagek@gmail.com>
2025-03-30 09:27:27 +0200

    .

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,7 +4,8 @@ import HomeProfileCard from "./HomeProfileCard";
 import Posts from "./Posts";
 
 const Home = () => {
-  return (<div>
+  return (
+  <div>
     <div className="flex justify-center space-x-12 mr-4">
       <div className="w-1/5 ml-4 mt-4">
         <HomeProfileCard />
@@ -17,7 +18,7 @@ const Home = () => {
       </div>
       <div className="w-1/5"></div>
     </div>
-    </div>
+  </div>
   );
 };
 

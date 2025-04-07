89bf64df15fd48a74929a11befa2963a57c1e0c8
hanaelsadek <167355010+hanaelsadek@users.noreply.github.com>
2025-04-07 15:31:57 +0200

    sideleftbar in network page

---
+++ src/pages/network/SideLeftBar.jsx
@@ -0,0 +1,106 @@
+import React, { useState } from 'react';
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  Typography,
+} from "@material-tailwind/react";
+
+const SidebarMenuItem = ({ icon, text, active, onClick }) => {
+  return (
+    <div 
+      className={`flex items-center px-4 py-3 cursor-pointer hover:bg-gray-100 ${
+        active ? 'bg-gray-100' : ''
+      }`}
+      onClick={onClick}
+    >
+      <div className="w-6 h-6 mr-3 text-gray-500 flex justify-center items-center">
+        {icon}
+      </div>
+      <span className="text-gray-700">{text}</span>
+    </div>
+  );
+};
+
+const SideLeftBar = () => {
+  const [activeItem, setActiveItem] = useState('Connections');
+  
+  const menuItems = [
+    { 
+      name: 'Connections', 
+      icon: (
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+          <path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z" />
+        </svg>
+      )
+    },
+    { 
+      name: 'Following & followers',
+      icon: (
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+          <path d="M8 9a3 3 0 100-6 3 3 0 000 6zM8 11a6 6 0 016 6H2a6 6 0 016-6zM16 7a1 1 0 10-2 0v1h-1a1 1 0 100 2h1v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1V7z" />
+        </svg>
+      )
+    },
+    { 
+      name: 'Groups',
+      icon: (
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+          <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v1h8v-1zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-1a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v1H1v-3a3 3 0 013.75-2.906z" />
+        </svg>
+      )
+    },
+    { 
+      name: 'Events',
+      icon: (
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+          <path fillRule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clipRule="evenodd" />
+        </svg>
+      )
+    },
+    { 
+      name: 'Pages',
+      icon: (
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+          <path fillRule="evenodd" d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4zm2 6a1 1 0 011-1h6a1 1 0 110 2H7a1 1 0 01-1-1zm1 3a1 1 0 100 2h6a1 1 0 100-2H7z" clipRule="evenodd" />
+        </svg>
+      )
+    },
+    { 
+      name: 'Newsletters',
+      icon: (
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+          <path fillRule="evenodd" d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z" clipRule="evenodd" />
+          <path d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z" />
+        </svg>
+      )
+    },
+  ];
+
+  return (
+    <Card className=" flex flex-start bg-white rounded-lg shadow border border-gray-200 w-64  border-top-left-radius-0.8
+        border-top-right-radius-0.8
+        border-bottom-right-radius-0.8
+        border-bottom-left-radius: 0.8 mt-4 ml-8">
+      
+       <CardBody className="py-2">
+       < Typography variant="h6" className="text-gray-800 mt-3 mb-3">Manage my network</Typography>
+       <div className="flex items-center my-1">
+          <div className="flex-grow border-t border-gray-300"></div>
+          <div className="flex-grow border-t border-gray-300"></div>
+        </div>
+        {menuItems.map((item) => (
+          <SidebarMenuItem 
+            key={item.name}
+            text={item.name}
+            icon={item.icon}
+            active={activeItem === item.name}
+            onClick={() => setActiveItem(item.name)}
+          />
+        ))}
+      </CardBody>
+    </Card>
+  );
+};
+
+export default SideLeftBar;

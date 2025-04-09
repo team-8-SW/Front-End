c2c78bbc5850670d5812b407f41f09c873c2888e
Mark-2005 <markmagednagek@gmail.com>
2025-04-09 22:48:02 +0200

    Added mark as read

--- src/pages/home/Posts/Posts.jsx
+++ src/pages/home/Posts/Posts.jsx
@@ -1,53 +1,28 @@
-import React, { useEffect, useState, useRef, useCallback } from "react";
+import React, { useEffect, useState } from "react";
 import PostDetails from "./PostDetails";
 import { fetchPosts } from "../../../services/api";
 
-const Posts = ({loggedUser}) => {
+const Posts = ({ loggedUser }) => {
   const [posts, setPosts] = useState([]);
-  const [page, setPage] = useState(1);
-  const observer = useRef();
-
-  const loadMorePosts = useCallback(async () => {
-    const newPosts = await fetchPosts(page);
-    setPosts((prevPosts) => [...prevPosts, ...newPosts]);
-    setPage((prevPage) => prevPage + 1);
-  }, [page]);
 
   useEffect(() => {
-    setPosts([]);
-    setPage(1);
-  }, []);
-
-  useEffect(() => {
-    const fetchInitialPosts = async () => {
-      const initialPosts = await fetchPosts(1);
-      setPosts(initialPosts);
-      setPage(2);
+    const fetchAllPosts = async () => {
+      try {
+        const allPosts = await fetchPosts();
+        setPosts(allPosts);
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+      }
     };
-    fetchInitialPosts();
-  }, []);
 
-  const lastPostElementRef = useCallback(
-    (node) => {
-      if (observer.current) observer.current.disconnect();
-      observer.current = new IntersectionObserver((entries) => {
-        if (entries[0].isIntersecting) {
-          loadMorePosts();
-        }
-      });
-      if (node) observer.current.observe(node);
-    },
-    [loadMorePosts]
-  );
+    fetchAllPosts();
+  }, []);
 
   return (
     <div>
-      {posts.map((post, index) => (
-        <div
-         
-          ref={index === posts.length - 1 ? lastPostElementRef : null}
-        >
-          <PostDetails post={post}  key={post.id} loggedUser={loggedUser}/>
+      {posts.map((post) => (
+        <div key={post.id}>
+          <PostDetails post={post} loggedUser={loggedUser} />
         </div>
       ))}
     </div>

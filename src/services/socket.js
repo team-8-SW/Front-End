65cbc634b4301d41206397e8fda134d43d119379
hanaelsadek <167355010+hanaelsadek@users.noreply.github.com>
2025-05-03 14:22:55 +0300

    .env sockets

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
  const token = localStorage.getItem('token');
-const socket = io('http://localhost:5000', {
+const socket = io(import.meta.env.VITE_SOCKET_URL, {
   transports: ['websocket'],
   autoConnect: true,
   auth: {
